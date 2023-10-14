ea89902669e899b3395034c2794699ea822b77e9
westone034626 <dydrlaks@likelion.org>
2023-10-14 14:44:13 +0900

    feat: Implement react router basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Route, Routes, Outlet } from 'react-router-dom';
+import { RegisterPage, LoginPage, LandingPage } from './pages';
+import { Footer, NavBar } from './layout';
+
+function Layout() {
+  return (
+    <div>
+      <NavBar />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold'>
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
