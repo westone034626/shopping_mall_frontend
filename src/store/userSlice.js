6832c6fee6e3e4bebea1231ad537c22cd01ca33f
westone034626 <dydrlaks@likelion.org>
2023-10-16 21:45:19 +0900

    feat: Implement Login page

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUser } from "./thunkFunctions";
+import { loginUser, registerUser } from "./thunkFunctions";
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -30,6 +30,21 @@ const userSlice = createSlice({
                 toast.info('회원가입을 성공했습니다.');
             })
             .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+
+                toast.error(action.payload);
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.userData = action.payload;
+                state.isAuth = true;
+                localStorage.setItem('accessToken', action.payload.accessToken);
+            })
+            .addCase(loginUser.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.payload;
 
