d28de63e48f45611691ca644eb893c070116aa4c
westone034626 <dydrlaks@likelion.org>
2023-11-03 22:59:55 +0900

    feat: Implement NavBar component

--- src/layout/NavBar/index.jsx
+++ src/layout/NavBar/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { NavItem } from './Sections';
 
 const NavBar = () => {
+    const [menu, setMenu] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu);
+    };
+
     return (
-        <div>NavBar</div>
+        <nav className='relative z-10 text-white bg-gray-900'>
+            <div className='w-full'>
+                <div className='flex items-center justify-between mx-5 sm:mx-10'>
+                    {/* Logo */}
+                    <div className='flex items-center text-2xl h-14'>
+                        <Link to={'/'}>Logo</Link>
+                    </div>
+
+                    {/* Menu button */}
+                    <div className='text-2xl sm:hidden'>
+                        <button onClick={handleMenu}>{menu ? "-" : "+"}</button>
+                    </div>
+
+                    {/* big screen nav-items */}
+                    <div className='hidden sm:block'>
+                        <NavItem />
+                    </div>
+                </div>
+
+                {/* Mobile nav-items */}
+                <div className='block sm:hidden'>
+                    {menu && <NavItem />}
+                </div>
+            </div>
+        </nav>
+
     );
 };
 
