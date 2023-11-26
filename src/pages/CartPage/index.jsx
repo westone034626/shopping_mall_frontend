e7ce5d468b9d2917cea1fc097058d90fc92e10a0
westone034626 <dydrlaks@likelion.org>
2023-11-26 18:02:41 +0900

    feat: Implement cart page UI

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCartDetail } from '../../store/thunkFunctions';
 
 const CartPage = () => {
     const dispatch = useDispatch();
     const userState = useSelector(state => state.user.userData);
+    const cartDetail = useSelector(state => state.user.cartDetail);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         if (userState?.cart && userState.cart.length > 1) {
@@ -17,8 +19,36 @@ const CartPage = () => {
         }
     }, [dispatch, userState]);
 
+    useEffect(() => {
+        calculateTotal(cartDetail);
+    }, [cartDetail]);
+
+    const calculateTotal = (cartItems) => {
+        const newTotal = cartItems.reduce((acc, cur) => acc + (cur.price * cur.quantity), 0);
+
+        setTotal(newTotal);
+    };
+
     return (
-        <div>CartPage</div>
+        <section>
+            <div className='text-center m-7'>
+                <h2 className='text-2xl'>
+                    나의 장바구니
+                </h2>
+            </div>
+
+            {cartDetail?.length > 0 ? (
+                <>
+                    <div className='mt-10'>
+                        <p><span className='font-bold'>합계: </span>{total}원</p>
+
+                        <button className='text-white bg-black hover:bg-gray-500 px-4 py-2 rounded-md mt-5'>
+                            결제하기
+                        </button>
+                    </div>
+                </>
+            ) : <p>장바구니가 비었습니다.</p>}
+        </section>
     );
 };
 
